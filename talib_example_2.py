a886ad2af5d774dacc27912c250fa8083beee209
Your Name <you@example.com>
2023-03-19 14:51:37 +0100

    1

--- talib_example_2.py
+++
@@ -1,6 +0,0 @@
-import talib as ta
-import yfinance as yf
-aapl = yf.download('AAPL', '2019-1-1','2023-03-01')
-aapl['Simple MA'] = ta.SMA(aapl['Close'],14)
-aapl['EMA'] = ta.EMA(aapl['Close'], timeperiod = 14)
-print(aapl.tail())
